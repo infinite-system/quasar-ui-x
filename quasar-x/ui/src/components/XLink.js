6f82b92f09f854edb2b5f550b2a1cd4592ef81a5
Evgeny Kalashnikov <ekalashnikov@gmail.com>
2022-11-08 12:54:20 -0500

    v0.1.2

---
+++ quasar-x/ui/src/components/XLink.js
@@ -0,0 +1,27 @@
+<script>export default { name: 'XLink'}</script>
+<script setup>
+import { computed } from 'vue'
+import { RouterLink } from 'vue-router'
+
+const props = defineProps({
+  ...RouterLink.props,
+  class : String
+})
+
+const isExternal = computed(() => {
+  return typeof props.to === 'string' && props.to.startsWith('http')
+})
+</script>
+<template>
+  <a v-if="isExternal"
+     target="_blank"
+     rel="noopener"
+     class="external-link"
+     v-bind="$attrs"
+     :href="to">
+    <slot/>
+  </a>
+  <router-link v-else v-bind="$props" class="internal-link" v-slot="useLink">
+    <slot v-bind="useLink"/>
+  </router-link>
+</template>
